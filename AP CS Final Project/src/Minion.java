510d5d5d0c65ca5339938f94c01b79aa919b2852
Akshay-Srivatsan <srivatsan.akshay@gmail.com>
2014-05-08 08:57:24 -0700

    Added Minion abstract class

---
+++ AP CS Final Project/src/Minion.java
@@ -0,0 +1,55 @@
+import java.awt.Point;
+
+
+public abstract class Minion
+{
+	private Point location;
+	private double direction;
+	private int health;
+	
+	public Minion()
+	{
+		location = new Point(-1,-1);
+		direction = -1;
+		health = -1;
+	}
+	
+	public Minion(Point _loc, double _dir, int _health)
+	{
+		location = _loc;
+		direction = _dir;
+		health = _health;
+	}
+	
+	public void setLocation(Point _location)
+	{
+		location = _location;
+	}
+	
+	public void setDirection(double _direction)
+	{
+		direction = _direction;
+	}
+	
+	public void setHealth(int _health)
+	{
+		health = _health;
+	}
+	
+	public Point getLocation()
+	{
+		return location;
+	}
+	
+	public int getHealth()
+	{
+		return health;
+	}
+	
+	public double getDirection()
+	{
+		return direction;
+	}
+	
+	
+}
