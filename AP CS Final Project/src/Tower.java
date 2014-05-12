2ae0aabae5c7fd5eac0c15ddfcdbe8dfef0e3c12
jcampbell <jcampbell@172.30.24.151>
2014-05-12 13:41:05 -0700

    kk

---
+++ AP CS Final Project/src/Tower.java
@@ -0,0 +1,21 @@
+
+public abstract class Tower
+{
+	private int health;
+	public Tower()	{}
+	
+	public int removeHealth()
+	{
+		
+	}
+	
+	public Minion spawn()
+	{
+		
+	}
+}
+
+public class meleeTower
+{
+	private int health = ;
+}
