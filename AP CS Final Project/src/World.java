d9e24cf7fabb7c0de0e6c0ee85456279324f5f65
shapneral <christensen_julian@yahoo.com>
2014-05-13 14:12:40 -0700

    WOrld

--- AP CS Final Project/src/World.java
+++ AP CS Final Project/src/World.java
@@ -3,7 +3,6 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
@@ -30,17 +29,25 @@ public class World extends JFrame implements ActionListener
 		Timer clock = new Timer(17, this);
         clock.start();
 	}
-	
-	public void paint(Graphics g)
-    {
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 22, gridWidth, gridHeight);
-    }
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) 
 	{
 		repaint();
 	}
+
+	public void paint(Graphics g)
+    {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 22, gridWidth, gridHeight);
+		for (Minion m : playerMinions)
+		{
+			m.draw(g);
+		}
+		for (Minion m : enemyMinions)
+		{
+			m.draw(g);
+		}
+    }
 	
 }
