7def5b2f5d9bb0a11657c5e6ffa7f2e0cf9c174a
Akshay-Srivatsan <srivatsan.akshay@gmail.com>
2014-05-15 09:34:52 -0700

    speed stuff

---
+++ AP CS Final Project/src/SplashMinion.java
@@ -0,0 +1,18 @@
+import java.awt.Point;
+import java.util.List;
+
+
+public class SplashMinion extends MeleeMinion
+{
+	private final int RANGE = 20;
+	public SplashMinion(Point loc, double dir, List<Minion> enemies)
+	{
+		super(loc, dir, enemies);
+	}
+	
+	public void attack()
+	{
+		for (Minion i )
+	}
+	
+}
